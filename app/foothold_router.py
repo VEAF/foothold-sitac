2488efa9e98976f02d9b6e5c77c29f23f5af6001
Michel NAUD <michel.naud26400@gmail.com>
2025-11-12 19:19:22 +0100

    add FOOTHOLD_SITAC_PATH

--- app/foothold_router.py
+++ app/foothold_router.py
@@ -1,3 +1,4 @@
+import os
 from fastapi import APIRouter, Request
 from fastapi.responses import HTMLResponse
 from app.foothold import load_sitac
@@ -7,11 +8,13 @@ env = Environment(loader=FileSystemLoader('templates'))
 
 router = APIRouter()
 
+FOOTHOLD_SITAC_PATH = os.getenv("FOOTHOLD_SITAC_PATH", "var/footholdSyria_Extended_0.1.lua")
+
 
 @router.get("", response_class=HTMLResponse)
 async def view_sitac(request: Request):
 
-    sitac = load_sitac("var/footholdSyria_Extended_0.1.lua")
+    sitac = load_sitac(FOOTHOLD_SITAC_PATH)
 
     template = env.get_template("sitac.html")
     return template.render(
