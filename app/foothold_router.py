b13d07fb528b90db874d7f47bf27f6e22d0bd29d
Michel NAUD <michel.naud26400@gmail.com>
2025-11-13 18:12:15 +0100

    add servers list

--- app/foothold_router.py
+++ app/foothold_router.py
@@ -1,25 +1,52 @@
-import os
-from fastapi import APIRouter, Request
+from fastapi import APIRouter, Request, HTTPException, status
 from fastapi.responses import HTMLResponse
-from app.foothold import load_sitac
+from app.foothold import detect_foothold_mission_path, get_server_path_by_name, list_servers, load_sitac
 from jinja2 import Environment, FileSystemLoader
 
 env = Environment(loader=FileSystemLoader('templates'))
 
 router = APIRouter()
 
-FOOTHOLD_SITAC_PATH = os.getenv("FOOTHOLD_SITAC_PATH", "var/footholdSyria_Extended_0.1.lua")
-
 
 @router.get("", response_class=HTMLResponse)
-async def view_sitac(request: Request):
+async def foothold_servers(request: Request):
 
-    sitac = load_sitac(FOOTHOLD_SITAC_PATH)
+    servers = list_servers()
 
-    template = env.get_template("sitac.html")
+    template = env.get_template("foothold/servers.html")
+    return template.render(
+        {
+            "request": request,
+            "servers": servers,
+        }
+    )
+
+
+@router.get("/sitac/{server}", response_class=HTMLResponse)
+async def foothold_sitac(request: Request, server: str):
+
+    server_path = get_server_path_by_name(server)
+
+    if not server_path.is_dir():
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"server {server} not found")
+
+    mission_path = detect_foothold_mission_path(server_path)
+
+    if not mission_path:
+        raise HTTPException(status.HTTP_404_NOT_FOUND, f"mission not found for server {server}")
+
+    sitac = load_sitac(mission_path)
+
+    template = env.get_template("foothold/sitac.html")
     return template.render(
         {
             "request": request,
             "sitac": sitac,
         }
     )
+
+
+@router.get("/ranking", response_class=HTMLResponse)
+async def view_ranking(request: Request):
+
+    raise Exception("TODO")
