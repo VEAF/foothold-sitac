7faa32b41f17722a89dc97a47b7d0c74404fd93c
Michel NAUD <michel.naud26400@gmail.com>
2025-12-14 20:52:17 +0100

    update hidden flag

--- tests/fixtures/test_hidden/Missions/Saves/foothold_hidden_test.lua
+++ tests/fixtures/test_hidden/Missions/Saves/foothold_hidden_test.lua
@@ -1,9 +1,15 @@
 zonePersistance = {}
 zonePersistance['zones'] = {
     ['VisibleZone1']={ ['upgradesUsed']=0,['side']=1,['active']=true,['destroyed']={ },['extraUpgrade']={ },['remainingUnits']={ [1]={ [1]='T-72B3' } },['lat_long']={ ['longitude']=36.0,['latitude']=33.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=1,['wasBlue']=false,['triggers']={ ['missioncompleted']=0 } },
-    ['VisibleZone2']={ ['upgradesUsed']=0,['side']=2,['active']=true,['destroyed']={ },['hidden']=false,['extraUpgrade']={ },['remainingUnits']={ },['lat_long']={ ['longitude']=37.0,['latitude']=34.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=2,['wasBlue']=true,['triggers']={ ['missioncompleted']=1 } },
-    ['HiddenZone1']={ ['upgradesUsed']=0,['side']=0,['active']=false,['destroyed']={ },['hidden']=true,['extraUpgrade']={ },['remainingUnits']={ },['lat_long']={ ['longitude']=38.0,['latitude']=35.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=0,['wasBlue']=false,['triggers']={ ['missioncompleted']=0 } },
-    ['HiddenZone2']={ ['upgradesUsed']=0,['side']=1,['active']=true,['destroyed']={ },['hidden']=true,['extraUpgrade']={ },['remainingUnits']={ },['lat_long']={ ['longitude']=39.0,['latitude']=36.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=3,['wasBlue']=false,['triggers']={ ['missioncompleted']=0 } }
+    ['VisibleZone2']={ ['upgradesUsed']=0,['side']=2,['active']=true,['destroyed']={ },['extraUpgrade']={ },['remainingUnits']={ },['lat_long']={ ['longitude']=37.0,['latitude']=34.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=2,['wasBlue']=true,['triggers']={ ['missioncompleted']=1 } },
+    ['HiddenZone1']={ ['upgradesUsed']=0,['side']=0,['active']=false,['destroyed']={ },['extraUpgrade']={ },['remainingUnits']={ },['lat_long']={ ['longitude']=38.0,['latitude']=35.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=0,['wasBlue']=false,['triggers']={ ['missioncompleted']=0 } },
+    ['HiddenZone2']={ ['upgradesUsed']=0,['side']=1,['active']=true,['destroyed']={ },['extraUpgrade']={ },['remainingUnits']={ },['lat_long']={ ['longitude']=39.0,['latitude']=36.0,['altitude']=0 },['firstCaptureByRed']=true,['level']=3,['wasBlue']=false,['triggers']={ ['missioncompleted']=0 } }
+}
+zonePersistance['zonesDetails'] = {
+    ['VisibleZone1']={ ['hidden']=false },
+    ['VisibleZone2']={ ['hidden']=false },
+    ['HiddenZone1']={ ['hidden']=true },
+    ['HiddenZone2']={ ['hidden']=true }
 }
 zonePersistance['playerStats'] = { }
 zonePersistance['accounts'] = { [1]=1000,[2]=2000 }
