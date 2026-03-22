810480eec88d203d0578f5e52516c7cfe2ca2c93
Michel NAUD <michel.naud26400@gmail.com>
2026-03-22 10:46:50 +0100

    parse weather info

---
+++ tests/fixtures/test_weather/Missions/Saves/foothold_weather.lua
@@ -0,0 +1,4 @@
+zonePersistance = {}
+zonePersistance['zones'] = { ['Zone1']={ ['upgradesUsed']=0,['side']=1,['active']=true,['destroyed']={ },['extraUpgrade']={ },['remainingUnits']={ },['firstCaptureByRed']=true,['level']=1,['wasBlue']=false,['lat_long']={ ['longitude']=54.0,['latitude']=26.0,['altitude']=0 },['triggers']={ ['missioncompleted']=0 } } }
+zonePersistance['playerStats'] = { }
+zonePersistance['weatherInfo'] = { ['windDirection']=240.99997522634,['windSpeed']=5.5,['temperature']=20.3,['pressure']=760.0,['cloudBase']=3000.0,['cloudDensity']=5,['fogVisibility']=10000.0 }
